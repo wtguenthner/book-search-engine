1aac147433ebfb6bd58839ccdbf4db720a1413b2
Taylor Guenthner <wtguenthner@gmail.com>
2022-10-05 18:42:26 -0400

    Added code for authorization

--- server/server.js
+++ server/server.js
@@ -4,14 +4,14 @@ const {ApolloServer} = require('apollo-server-express')
 const db = require('./config/connection');
 
 const {typeDefs, resolvers} = require('./schemas');
-// const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  // context: authMiddleware,
+  context: authMiddleware,
 })
 
 app.use(express.urlencoded({ extended: true }));
